caefaa47c759b4d58e6db66d0b47ba2c48ab0f78
itsBunny07 <bunnysmail4fun@gmail.com>
2024-06-14 23:23:51 +0530

    Add files via upload

---
+++ OSINT/request.py
@@ -0,0 +1,33 @@
+import requests
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
+
+class OSINT:
+    def OSINTTool(self, username):
+        websites = {
+            "Instagram" : "https://instagram.com/",
+            "Github" : "https://github.com/",
+            "Facebook" : "https://facebook.com/",
+            "TryHackMe" : "https://tryhackme.com/p/",
+            "LeetCode" : "https://leetcode.com/u/",
+            "Twitter / X" : "https://x.com/"
+        }
+
+
+        for key, value in websites.items():
+            req = requests.get(value + username)
+            
+            if req.status_code == 200:
+                print(Fore.GREEN + f'Account Found : {key} : {value + username}')
+                
+            elif req.status_code == 404:
+                print(Fore.RED + f"404 Account Not Found : {key}")
+                
+            else:
+                print(Fore.BLUE + f"Error finding account on {key} : Status Code : {req.status_code}")
+                
+request = OSINT()
+
+username = input("Enter the username to be examined : ")
+request.OSINTTool(username)
